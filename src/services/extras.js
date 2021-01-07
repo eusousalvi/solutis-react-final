8659fb0a60dd6808b18e58f68c38eef6b4b9fe0b
Vitor M Mendonça <monteiromendonca@gmail.com>
2021-01-07 16:30:38 -0300

    Config extras api connection

---
+++ src/services/extras.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://5ff71a40e7164b0017e1a256.mockapi.io",
+});
+
+async function getExtras() {
+  let data = true;
+
+  await api
+    .get("/extras")
+    .then(response => {
+      if (response) data = response.data;
+    })
+    .catch(err => console.log("Error: ", err));
+
+  return data;
+}
+
+async function deleteExtra(id) {
+  let data = false;
+
+  await api
+    .delete(`/extras/${id}`)
+    .then(response => {
+      if (response) data = true;
+    })
+    .catch(err => {
+      console.log("Err", err);
+    });
+
+  return data;
+}
+
+export default { getExtras, deleteExtra };
