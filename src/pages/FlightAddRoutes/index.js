6e8335e038674990f809cc4ef866c3fab6b09346
JoaoP <contato.joaocomp@gmail.com>
2021-01-08 13:01:54 -0300

    Fixing field validation

--- src/pages/FlightAddRoutes/index.js
+++ src/pages/FlightAddRoutes/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-
 import axios from "axios";
 
+import { useHistory } from "react-router-dom";
+
 import FlightRouteForm from "../../components/FlightRoutesComponents/FlightRouterForm";
-import Header from "../../components/Header";
 
 export default function AddFlightRoutes() {
+  const history = useHistory();
+
   function handleFormSubmit(values) {
     axios
       .post("https://5ff83d6510778b0017042ff3.mockapi.io/routes/", values)
+      .then(() => history.push("/flights/routes"))
       .catch((erro) => console.log(erro));
-
-    console.log(values);
   }
 
   return (
