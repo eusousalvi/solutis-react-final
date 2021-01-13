f034021b3dfffc0f6b42d6d98a5fa833935731ab
Luca Agra <luca.agra@hotmail.com>
2021-01-13 17:55:36 -0300

    Resolve Conflicts

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,21 +3,15 @@ import thunk from "redux-thunk";
 import exampleReducer from "./reducers/exampleReducer";
 import formFlightRoutesReducer from "./reducers/formFlightRoutesReducer";
 import rooms from "./reducers/rooms";
-<<<<<<< HEAD
 import hotelsReducer from "./reducers/hotelsReducer";
-=======
 import airports from "./reducers/airports";
->>>>>>> d0b369593fa5ab591b81ecf71b86ba2ac270a4ab
 
 const reducer = combineReducers({
   formFlightRoutesReducer,
   exampleReducer,
   rooms,
-<<<<<<< HEAD
   hotelsReducer,
-=======
-  airports
->>>>>>> d0b369593fa5ab591b81ecf71b86ba2ac270a4ab
+  airports,
 });
 
 const middleware = applyMiddleware(thunk);
