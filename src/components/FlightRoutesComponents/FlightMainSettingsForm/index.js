7cb4d63776f3121711fb276877959759b22966fe
JoaoP <contato.joaocomp@gmail.com>
2021-01-14 15:00:13 -0300

    Fixing undefined property on route node

--- src/components/FlightRoutesComponents/FlightMainSettingsForm/index.js
+++ src/components/FlightRoutesComponents/FlightMainSettingsForm/index.js
@@ -5,9 +5,9 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { update } from "../../../redux/actions/formFlightRoutes";
 
-function parseNodeDate(node) {
+function parseNodeDate(node, arrival = false) {
   const date = node.date.split("-");
-  const time = node.time.split(":");
+  const time = arrival ? node.checkin.split(":") : node.checkout.split(":");
   return new Date(date[0], date[1] - 1, date[2], time[0], time[1]);
 }
 
@@ -18,7 +18,7 @@ function minutesToString(minutes) {
 }
 
 function getNodeDifference(initial, final) {
-  return (parseNodeDate(final) - parseNodeDate(initial)) / (1000 * 60);
+  return (parseNodeDate(final) - parseNodeDate(initial, true)) / (1000 * 60);
 }
 
 export default function MainSettingsForm() {
